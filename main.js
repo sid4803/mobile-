85d75cfe941d524382c1b0a1863a27fb6cad1fb0
sid4803 <84769771+sid4803@users.noreply.github.com>
2021-07-03 13:05:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+    constraints:{
+        facingMode:"environment"
+    }
+})
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>';        
+    });
+}
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+function modelLoaded(){
+    console.log("modelloaded");
+}
+function  check(){
+img=document.getElementById("capture_image");
+classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label;
+}
+}
